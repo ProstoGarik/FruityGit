19714383994bf6c35b40649dc7238f74d5c478fb
11LEGION11 <bbanan570@gmail.com>
2025-04-11 20:32:23 +0300

    WIP repo dir

--- FruityGitDesktop/FruityGitServer/Controllers/MessagesController.cs
+++ FruityGitDesktop/FruityGitServer/Controllers/MessagesController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Http;
 [Route("api/git")]
 public class GitController : ControllerBase
 {
-    private readonly string _repoPath = @"../../ReposFolder";
+    private readonly string _repoPath = @"../../../ReposFolder";
 
     [HttpPost("init")]
     public IActionResult InitializeRepository()
